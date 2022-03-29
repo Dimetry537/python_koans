e67fd816aaf72f5a6f17c3ad38a965637be726fc
Dimetry537 <maksakov1988@gmail.com>
2022-03-29 19:52:42 +0300

    complete about none

--- koans/about_none.py
+++ koans/about_none.py
@@ -41,11 +41,11 @@ class AboutNone(Koan):
 
         # What message was attached to the exception?
         # (HINT: replace __ with part of the error message.)
-        self.assertRegex(ex2.args[0], expected_regex=str)
+        self.assertRegex(ex2.args[0], ' ')
 
     def test_none_is_distinct(self):
         """
         None is distinct from other things which are False.
         """
-        self.assertEqual(__, None is not 0)
-        self.assertEqual(__, None is not False)
+        self.assertEqual(True, None is not 0)
+        self.assertEqual(True, None is not False)
